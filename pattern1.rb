0773e60f9c3b4e0a23e87ccbc764d7101e78994c
muttushingadi <muttu.singadi@gmail.com>
2016-05-27 18:54:10 +0530

    other programs

--- pattern1.rb
+++ pattern1.rb
@@ -1,8 +1,14 @@
+###To print the following pattern
+# 1
+# 11
+# 111
+# 1111
+########
 class PrintAllOnes
-for line in 0..3
-for count in 1..line+1
-print "1"
-end
-puts "\n"
-end
+  for line in 1..4
+     for count in 1..line
+       print "1"
+     end
+	puts "\n"
+  end
 end
